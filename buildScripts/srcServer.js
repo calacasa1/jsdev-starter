14d7c2b6ffe9d03c152616e5ae979c57af24bc1b
Lacasa <clacasa@emphasys.local>
2018-08-03 13:02:00 -0400

    added http calls and mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,16 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+    res.json([
+        {"id": 1, "firstname": "Bob", "lastname": "Smith", "email": "bob@gmail.com"},
+        {"id": 2, "firstname": "Swaggy", "lastname": "C", "email": "swaggy@gmail.com"},
+        {"id": 3, "firstname": "Rockstar", "lastname": "Jones", "email": "rockstar@gmail.com"},
+        {"id": 4, "firstname": "Brett", "lastname": "Bro", "email": "brett@gmail.com"},
+        {"id": 5, "firstname": "Tyler", "lastname": "Durden", "email": "tyler@gmail.com"},
+    ]);
+});
+
 app.listen(port, function(err) {
     if(err) { console.log(err); }
     else {
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './index.css';
+import { getUsers, deleteUser } from './api/userApi';
+
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user  => {
+        usersBody += `<tr>
+            <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstname}</td>
+            <td>${user.lastname}</td>
+            <td>${user.email}</td>
+            </tr>`
+    });
+
+
+    global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+    
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const ele = event.target;
+            event.preventDefault();
+            deleteUser(ele.attributes["data-id"].value);
+            const row = ele.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
+});
+
